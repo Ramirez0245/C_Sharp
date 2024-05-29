5c3d4e675ed3b7f246a6ac757dbe0a15bac0b8cc
ramirez0245 <ramirez000245@gmail.com>
2024-05-28 22:04:54 -0700

    3rd

--- commit_run.py
+++ commit_run.py
@@ -1,10 +1,25 @@
-import os
+import subprocess
+placeholder = input()
+ACTIONS = [
+    ["git","add", ".", "commit"],
+    ["git", "commite", "-m", placeholder],
+    ["git", "branch", "-M", "main"],
+    ["git", "push", "-u", "origin", "orgin/main"]
+    ]
 
-commit_name = input()
-
-def commite(commit_name):
+for command in ACTIONS:
     print("Hello world")
-    os.system("git add . | git commit  ")
+    print(command)
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    print(result.stdout)
+
+# commit_name = input()
+
+# def commite(commit_name):
+#     print("Hello world")
+
+#     subprocess.run(["ls", "-l"])
+#     # subprocess.run(["git add .", f'{commit_name}', 'git branch -M main', 'git push -u origin main'])
     
 
-commite(commit_name)
+# commite(commit_name)
